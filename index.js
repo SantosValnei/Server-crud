1495b95a0247b423e803ccd3bb98dc207c94bd0a
SantosValnei <valneisilvaa@hotmail.com>
2022-05-11 15:57:19 -0300

    Adicionei o adicionar e o listar

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql2');
+const port = 8080;
+const cors = require('cors');
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "crud-simples"
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const { name } = req.body;
+
+    let SQL = "INSERT INTO pessoas ( name ) VALUES (?)";
+
+    db.query(SQL, [name], (err, result) => {
+        console.log(err)
+    })
+});
+
+app.get("/listar", (req, res) => {
+    
+    let SQL = "SELECT * FROM pessoas";
+
+    db.query(SQL, (err, result) => {
+        if (err) console.log(err);
+        else res.send(result);
+    })
+})
+
+app.listen(port)
